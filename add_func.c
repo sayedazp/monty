3d8194df14cde10cf811b1e019bfe877946e96dd
sayed azp <sayedazp@gmail.com>
2023-06-23 04:16:15 +0300

    task5

--- add_func.c
+++ add_func.c
@@ -30,3 +30,14 @@ void f_add(stack_t **head, unsigned int counter)
 	*head = h->next;
 	free(h);
 }
+/**
+  *f_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}

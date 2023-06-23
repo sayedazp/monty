3550e801737843ee863eb315f3b731733ff71032
sayed azp <sayedazp@gmail.com>
2023-06-23 04:10:21 +0300

    task3

--- assigner.c
+++ assigner.c
@@ -12,6 +12,7 @@ int assign(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall},
 				{"pint", f_pint},
+				{"pop",f_pop},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;

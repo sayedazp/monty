f9b4746351cb4dcad0a970acd3daaf30eb3bd0df
sayed azp <sayedazp@gmail.com>
2023-06-23 03:59:39 +0300

    desperate

--- assigner.c
+++ assigner.c
@@ -4,22 +4,22 @@
 * assign - executes the opcode
 * @stack: head linked list - stack
 * @counter: line_counter
+* @file: poiner to monty file
+* @content: line content
 * Return: state integer
 */
-int assign(stack_t **stack, unsigned int counter)
-{
-	instruction_t opst[] = {
-				{"push", f_push},
-				{"pall", f_pall},
+int assign(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
 	char *op;
 
-	op = strtok(drivers.content, " \n\t");
+	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
-	drivers.arg = strtok(NULL, " \n\t");
+	bus.arg = strtok(NULL, " \n\t");
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
@@ -29,12 +29,10 @@ int assign(stack_t **stack, unsigned int counter)
 		i++;
 	}
 	if (op && opst[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-		fclose(drivers.file);
-		free(drivers.content);
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
 		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
+		exit(EXIT_FAILURE); }
 	return (1);
 }
--- main.c
+++ main.c
@@ -1,44 +1,45 @@
 #include "monty.h"
-#define  _GNU_SOURCE
-#include <stdio.h>
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - entry point func
-* @argc: arguements count
-* @argv: pointer of arguments
-* Return: integer declaring the state
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
 */
-driver_t drivers = {NULL, NULL, NULL};
 int main(int argc, char *argv[])
 {
-	driver_t drivers = {NULL, NULL, NULL};
 	char *content;
 	FILE *file;
-	ssize_t lineRead = 1, size;
-	unsigned int cnt = 0;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-	if (argc != 2)
+	if (argc == 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	drivers.file = file;
+	file = fopen("0.m", "r");
+	bus.file = file;
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	set_file(file);
-	while (lineRead > 0)
+	while (read_line > 0)
 	{
-		drivers.content = NULL;
-		lineRead = getline(&content, &size, file);
-		set_content(content);
-		cnt++;
-		if (lineRead > 0)
-			assign(&stack, cnt);
-		free(drivers.content);
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			assign(content, &stack, counter, file);
+		}
+		free(content);
 	}
-	return (0);
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
